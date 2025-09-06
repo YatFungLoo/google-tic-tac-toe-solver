e90bbf1e76c7b9085165c6248c39918c73518b42
YatFungLoo <looyatfung@gmail.com>
2025-09-07 00:37:52 +0800

    working board reader

--- bot.py
+++ bot.py
@@ -5,8 +5,9 @@ import random
 
 
 RESET_DURATION = 0.3  # in seconds
+BUFFER_DURATION = 0.2  # in seconds
 GRID_POS = [0.15, 0.5, 0.85]  # gird scaling
-OFFSET_DEF_ZOOM = 0.16  # find pixel
+OFFSET_DEF_ZOOM = 0.35  # find pixel
 RETINA_FACTOR = 2  # for MacBook
 CROSS_COLOUR = (84, 84, 84)  # 545454
 CIRCLE_COLOR = (241, 235, 213)  # F1EBD5
@@ -19,7 +20,7 @@ def main():
     except ag.ImageNotFoundException:
         print("restart button not found on screen.")
         sys.exit(0)
-    restartCoord = [coord / 2 for coord in restartButt]
+    restartCoord = [coord / RETINA_FACTOR for coord in restartButt]
 
     # focus and reset board
     ag.doubleClick(restartCoord[0], restartCoord[1])
@@ -64,30 +65,43 @@ def main():
 
     # adjust for retina / add offset
     offset = (board.width / RETINA_FACTOR) / 3 * OFFSET_DEF_ZOOM
-    boardCoord = [[(coord / RETINA_FACTOR - offset) for coord in sublist]
+    boardWithFactor = [[(coord - offset) for coord in sublist]  # for pixel matching
+                       for sublist in boardCoord]
+    boardCoord = [[(coord / RETINA_FACTOR) for coord in sublist]
                   for sublist in boardCoord]
 
+    # test
     ag.click(boardCoord[0][0], boardCoord[0][1])
-    time.sleep(1)
+    time.sleep(BUFFER_DURATION)
     ag.click(boardCoord[1][0], boardCoord[1][1])
+    time.sleep(BUFFER_DURATION)
+    ag.click(boardCoord[4][0], boardCoord[4][1])
+    time.sleep(BUFFER_DURATION)
+    ag.click(boardCoord[6][0], boardCoord[6][1])
+    time.sleep(BUFFER_DURATION)
+    ag.click(boardCoord[7][0], boardCoord[7][1])
+    time.sleep(BUFFER_DURATION)
 
     # main loop
-    region = [coord / RETINA_FACTOR for coord in board]
-    pix_circle = ag.pixel(boardCoord[0][0], boardCoord[0][1])
-    print(pix_circle)
-    print(CIRCLE_COLOR)
-    pix_cross = ag.pixel(boardCoord[1][0], boardCoord[1][1])
-    print(pix_cross)
-    print(CROSS_COLOUR)
-    pix_empty = ag.pixel(boardCoord[2][0], boardCoord[2][1])
-    print(pix_empty)
-    print(BG_COLOUR)
-    if pix_circle == CIRCLE_COLOR:
-        print("found circle")
-    if pix_cross == CROSS_COLOUR:
-        print("found cross")
-    if pix_empty == BG_COLOUR:
-        print("found background")
+    # region = [coord / RETINA_FACTOR for coord in board]
+
+    current = []
+    for coord in boardWithFactor:
+        if (ag.pixelMatchesColor(coord[0], coord[1], CIRCLE_COLOR, tolerance=10)):
+            current.append("o")
+            continue
+        if (ag.pixelMatchesColor(coord[0], coord[1], CROSS_COLOUR, tolerance=10)):
+            current.append("x")
+            continue
+        if (ag.pixelMatchesColor(coord[0], coord[1], BG_COLOUR, tolerance=10)):
+            current.append("_")
+            continue
+        print("error reading board")
+        sys.exit(0)
+
+    print(current[0], current[1], current[2])
+    print(current[3], current[4], current[5])
+    print(current[6], current[7], current[8])
     sys.exit(0)
 
     while 1:
